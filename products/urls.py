570e3b3929cfd58f1eabb30068736eedaea91090
aavasylenko <aavasylenko@hotmail.com>
2020-11-24 21:56:46 +0000

    Finished Products page

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+]
